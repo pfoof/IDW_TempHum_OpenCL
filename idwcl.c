9b13201d1a142d0d051f4e293bef50168b5ea488
Pfoof <rotworm0@yahoo.pl>
2019-11-09 16:32:44 +0100

    First verison of C program for OpenCL

---
+++ idwcl.c
@@ -0,0 +1,159 @@
+#define PROGRAM_FILE "projekt_opencl.cl"
+#define KERNEL_FUNC "idw2"
+#define COLOR_FUNC "colorize"
+
+#include <CL/cl.h>
+#include <stdio.h>
+
+const int output_size[2] = { 1024, 1024 };
+
+cl_device_id create_device() {
+
+   cl_platform_id platform;
+   cl_device_id dev;
+   int err;
+
+   /* Identify a platform */
+   err = clGetPlatformIDs(1, &platform, NULL);
+   if(err < 0) {
+      perror("Couldn't identify a platform");
+      exit(1);
+   } 
+
+   // Access a device
+   // GPU
+   err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &dev, NULL);
+   if(err == CL_DEVICE_NOT_FOUND) {
+      // CPU
+      err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_CPU, 1, &dev, NULL);
+   }
+   if(err < 0) {
+      perror("Couldn't access any devices");
+      exit(1);   
+   }
+
+   return dev;
+}
+
+cl_program build_program(cl_context ctx, cl_device_id dev, const char* filename) {
+
+   cl_program program;
+   FILE *program_handle;
+   char *program_buffer, *program_log;
+   size_t program_size, log_size;
+   int err;
+
+   /* Read program file and place content into buffer */
+   program_handle = fopen(filename, "r");
+   if(program_handle == NULL) {
+      perror("Couldn't find the program file");
+      exit(1);
+   }
+   fseek(program_handle, 0, SEEK_END);
+   program_size = ftell(program_handle);
+   rewind(program_handle);
+   program_buffer = (char*)malloc(program_size + 1);
+   program_buffer[program_size] = '\0';
+   fread(program_buffer, sizeof(char), program_size, program_handle);
+   fclose(program_handle);
+
+   /* Create program from file 
+   Creates a program from the source code in the add_numbers.cl file. 
+   Specifically, the code reads the file's content into a char array 
+   called program_buffer, and then calls clCreateProgramWithSource.
+   */
+   program = clCreateProgramWithSource(ctx, 1, 
+      (const char**)&program_buffer, &program_size, &err);
+   if(err < 0) {
+      perror("Couldn't create the program");
+      exit(1);
+   }
+   free(program_buffer);
+
+   /* Build program 
+   The fourth parameter accepts options that configure the compilation. 
+   These are similar to the flags used by gcc. For example, you can 
+   define a macro with the option -DMACRO=VALUE and turn off optimization 
+   with -cl-opt-disable.
+   */
+   err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+   if(err < 0) {
+
+      /* Find size of log and print to std output */
+      clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG, 
+            0, NULL, &log_size);
+      program_log = (char*) malloc(log_size + 1);
+      program_log[log_size] = '\0';
+      clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG, 
+            log_size + 1, program_log, NULL);
+      printf("%s\n", program_log);
+      free(program_log);
+      exit(1);
+   }
+
+   return program;
+}
+
+int main() {
+
+
+   /* OpenCL structures */
+   cl_device_id device;
+   cl_context context;
+   cl_program program;
+   cl_kernel kernel_idw, kernel_colorize;
+   cl_command_queue queue;
+   cl_int err;
+
+   device = create_device();
+   context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
+   if(err < 0) {
+      perror("Couldn't create a context");
+      exit(1);   
+   }
+
+   /* Build program */
+   program = build_program(context, device, PROGRAM_FILE);
+
+   queue = clCreateCommandQueue(context, device, 0, &err);
+   if(err < 0) {
+      perror("Couldn't create a command queue");
+      exit(1);   
+   };
+
+   /* Create kernels */
+   kernel_idw = clCreateKernel(program, KERNEL_FUNC, &err);
+   if(err < 0) {
+      perror("Couldn't create a kernel idw");
+      exit(1);
+   };
+   kernel_colorize = clCreateKernel(program, COLORIZE_FUNC, &err);
+   if(err < 0) {
+      perror("Couldn't create a kernel colorize");
+      exit(1);
+   };
+
+    float* output = (float*)malloc(output_size[0]*output_size[1]*sizeof(float));
+    cl_mem output_buffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_COPY_HOST_PTR, output_size[0]*output_size[1]*sizeof(float), output, &err);
+
+   if(err < 0) {
+      perror("Error creating buffer");
+      exit(1);
+   };
+
+    cl_mem input_buffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, output_size[0]*output_size[1]*sizeof(float), output, &err);
+
+   if(err < 0) {
+      perror("Error creating buffer");
+      exit(1);
+   };
+
+    
+
+   clReleaseKernel(kernel_idw);
+   clReleaseKernel(kernel_colorize);
+   clReleaseCommandQueue(queue);
+   clReleaseProgram(program);
+   clReleaseContext(context);
+   return 0;
+}
