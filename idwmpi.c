75339f5b6931f2650219b18be6fc0716278a5cf9
Piotr <piotr@core>
2019-11-11 18:57:52 +0100

    Getting processor counts from slaves

--- idwmpi.c
+++ idwmpi.c
@@ -1,14 +1,48 @@
 #include <stdio.h>
 #include <openmpi/mpi.h>
 #include <sys/sysinfo.h>
+#include "input.h"
+
+#define TAG_NPROC 100
 
 unsigned int processors = 1;
 
+void idw2(float* _input, int* _input_size, float* output, int* output_size) {
+
+}
+
 int main(int argc, char* argv[]) {
     processors = get_nprocs();
-    MPI_Init(argc, argv);
+    MPI_Init(&argc, &argv);
 
-    
+    int tid, nthreads;
+    MPI_Comm_rank(MPI_COMM_WORLD, &tid);
+
+    if(tid == 0) {
+        
+        //Main Thread
+        MPI_Comm_size(MPI_COMM_WORLD, &nthreads);
+        
+        printf("Getting processor counts...\n");
+        int cores[nthreads], allcores;
+        cores[0] = processors; allcores = processors;
+        int i;
+        for(i = 0; i < nthreads - 1; ++i) {
+            int r; MPI_Status recvStatus;
+            MPI_Recv(&r, 1, MPI_INT, MPI_ANY_SOURCE, TAG_NPROC, MPI_COMM_WORLD, &recvStatus);
+            cores[recvStatus.MPI_SOURCE] = r;
+            allcores += r;
+        }
+        printf("Total of %d cores int %d processes\n", allcores, nthreads);
+        
+        
+
+        //end tid == 0
+    } else {
+        
+        //Other threads
+        MPI_Send(&processors, 1, MPI_INT, 0, TAG_NPROC, MPI_COMM_WORLD);
+    }
 
     MPI_Finalize();
     return 0;
